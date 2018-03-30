bef1ce9d1d3dc2a9aba6c897360f1e8b330c1329
TsukiZombina <ammy_zmmn@hotmail.com>
2018-03-29 21:15:48 -0600

    Delete output.jpg and rw.h, modify aesni.h to and main.c Add functions double_AES_enc, double_AES_dec and random_keygen

--- util.h
+++ util.h
@@ -1,8 +1,23 @@
-#ifndef RW_H
-#define RW_H
+#ifndef UTIL_H
+#define UTIL_H
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <wmmintrin.h>
+
+void print128_num(__m128i var) 
+{
+    int64_t *v64val = (int64_t*) &var;
+    printf("%.16lx%.16lx\n", v64val[0], v64val[1]);
+}
+
+void printhex(const unsigned char *out, long length) {
+    for(int i = 0; i < length; i++)
+    {
+        printf("%02x", out[i]);
+    }
+}
 
 unsigned char* read_file(const char* filename, long* size)
 {
@@ -33,4 +48,14 @@ void write_file(const char* filename, unsigned char* data, long size)
     fclose(outputStream);
 }
 
-#endif // RW_H
+void random_keygen(unsigned char* userkey0, unsigned char* userkey1) {
+    time_t t;
+    srand((unsigned) time(&t));
+
+    for (int i = 0; i < 16; i++) {
+        userkey0[i] = rand() % 2;
+        userkey1[i] = rand() % 2;
+    }
+}
+
+#endif // UTIL_H
